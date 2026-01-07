d2bd38c504f5c2401cd9c8bd2114e7a29b0fb377
branc4aaa <ebrancatti@gmail.com>
2026-01-06 23:03:19 -0300

    fix

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('projs/', views.proj_view, name='projects'),
     path('about/<int:pid>', views.about_view, name='about'),
 ]
